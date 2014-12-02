1f27f1743350f8289051dd47808e8ebd03556139
RamkhanaCh <katzeyj@live.com>
2014-12-02 15:50:23 +0700

    2

--- spec/lib/lovemac_spec.rb
+++ spec/lib/lovemac_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 
 describe "#lovemac(number)" do
 	#Describe Method
-	it "Test if you love mac" do
-		#Describe method scenario
+	it "Test love mac" do
+		
 		status = lovemac(50);
 		expect(status).to eq(50)
 	end
